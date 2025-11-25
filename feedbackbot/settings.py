4812709338dfa707504af4fe5449ea9f91abe240
AMB Team <>
2025-11-25 22:37:31 +0100

    AMB-31: форматирование настроек

--- feedbackbot/settings.py
+++ feedbackbot/settings.py
@@ -7,11 +7,15 @@ load_dotenv()
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
+
 # Paths
+
 ROOT_PATH = Path(__file__).resolve().parents[1]
 TMP_PATH = ROOT_PATH / 'data' / 'tmp'
 
+
 # Logging
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -46,25 +50,32 @@ LOGGING = {
     },
 }
 
+
 # DB
+
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_URI = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-print('DB URI:', DB_URI)
+
 
 # Commands
+
 COMMANDS = {
     'createdb': 'feedbackbot.core.commands.create_db',
     'cleandb': 'feedbackbot.core.commands.clean_db',
 }
 
+
 # Telegram
+
 TOKEN = os.getenv('TG_TOKEN')
 BOT_ID = int(TOKEN.split(':')[0]) if TOKEN else None
 CHAT_ID = os.getenv('TG_CHAT_ID')
 
+
 # Sentry
+
 SENTRY_DSN = os.getenv('SENTRY_DSN')
